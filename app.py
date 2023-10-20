67b6e6860cd6fe785f88a1d4a03bcf94cca23802
RayVilaca <cecilia_vilaca@outlook.com>
2023-10-20 17:16:18 -0300

    feat: add app

---
+++ app.py
@@ -0,0 +1,31 @@
+
+import pickle
+import warnings
+from flask import Flask, render_template, request, jsonify
+
+warnings.filterwarnings("ignore", category=UserWarning)
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/prever', methods=['POST'])
+def prever():
+    
+    arq = open('modelo_preco_imovel.pkl','rb')
+    modelo = pickle.load(arq)
+    arq.close()
+    
+    data = request.get_json()
+
+    # Processa os dados do formulário e faz previsões usando o modelo
+    # Retorna as previsões como JSON
+    resultado = modelo.predict([data])[0]
+    
+    if resultado < 0.0:
+        resultado = 0.0
+    
+    return jsonify({'previsao': resultado})
+
